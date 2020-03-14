e092784a9a34a6d930b13e74b3de0f7417fdf9ef
Joni <joni.suominen98@gmail.com>
2020-03-14 13:31:52 +0200

    heatmap

--- src/components/Map.js
+++ src/components/Map.js
@@ -8,8 +8,17 @@ import {
   GeoJSON
 } from "react-leaflet";
 import DataMarker from "./DataMarker";
+import Choropleth from "react-leaflet-choropleth";
 var data = require("../data/districtmap.json");
 var geojson = require("../data/shpNew.json");
+const style = {
+  fillColor: "#F28F3B",
+  weight: 2,
+  opacity: 1,
+  color: "white",
+  dashArray: "3",
+  fillOpacity: 0.6
+};
 
 class Map extends React.Component {
   state = {
@@ -21,6 +30,17 @@ class Map extends React.Component {
   componentDidMount() {
     coronaService.getAllInfection().then(confirmed => {
       console.log(confirmed);
+      Object.keys(confirmed).forEach(function(key) {
+        var idx = geojson.features.findIndex(g =>
+          g.properties.name == key
+        );
+        console.log(idx)
+        if (idx !== -1) {
+          geojson.features[idx].properties.count = confirmed[key].infections
+        }
+        console.log(geojson.features)
+      });
+
       this.setState({ infections: confirmed });
     });
   }
@@ -47,24 +67,33 @@ class Map extends React.Component {
         easeLinearity={0.35}
       >
         <TileLayer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png" />
-
-        {this.state.infections != null
-          ? Object.keys(this.state.infections).map(function(key) {
-              console.log(key)
-              return (
-                <DataMarker
-                  x={data[key].x}
-                  y={data[key].y}
-                  name={key}
-                  infections={all[key].infections}
-                  deaths={all[key].deaths}
-                  recovered={all[key].recovered}
-                />
-              );
-            })
-          : console.log("hei")}
-
-        <GeoJSON key={`geojson-01`} data={geojson} />
+        <Choropleth
+          data={geojson}
+          valueProperty={feature => feature.properties.count}
+          scale={["#d14900", "#d10000"]}
+          steps={7}
+          mode="e"
+          style={style}
+          onEachFeature={(feature, layer) =>
+            layer.bindPopup(feature.properties.label)
+          }
+        >
+          {this.state.infections != null
+            ? Object.keys(this.state.infections).map(function(key) {
+                console.log(key);
+                return (
+                  <DataMarker
+                    x={data[key].x}
+                    y={data[key].y}
+                    name={key}
+                    infections={all[key].infections}
+                    deaths={all[key].deaths}
+                    recovered={all[key].recovered}
+                  />
+                );
+              })
+            : console.log("hei")}
+        </Choropleth>
       </LeafletMap>
     );
   }
