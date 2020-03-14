3fe0c0cb5d4bb78b6e00a86170e5c14912401ad4
Joni <joni.suominen98@gmail.com>
2020-03-14 16:47:16 +0200

    page layout

--- src/pages/MapPage.js
+++ src/pages/MapPage.js
@@ -1,6 +1,9 @@
 import React from "react";
 import coronaService from "../services/corona";
 import Map from "../components/Map";
+import { withStyles } from "@material-ui/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
 
 var data = require("../data/districtmap.json");
 var geojson = require("../data/shpNew.json");
@@ -9,6 +12,17 @@ const bounds = [
   [71.035597, 18.032228],
   [59.269625, 33.105469]
 ];
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    padding: 4,
+    textAlign: "center",
+    color: "white"
+  }
+});
 
 class MapPage extends React.Component {
   state = {
@@ -24,8 +38,20 @@ class MapPage extends React.Component {
   }
 
   render() {
-    return <Map></Map>;
+    const { classes } = this.props;
+    return (
+      <div>
+        <Grid container>
+          <Grid item xs={9}>
+            <Map></Map>
+          </Grid>
+          <Grid item xs={3}>
+            <Paper className={classes.paper}>xs=12 sm=6</Paper>
+          </Grid>
+        </Grid>
+      </div>
+    );
   }
 }
 
-export default MapPage;
+export default withStyles(styles)(MapPage);
